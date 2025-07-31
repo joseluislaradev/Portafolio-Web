fc2d8e00c105e39b87ddfdab96df4a54ffbc0e58
JoseLuis1411 <perezlarajose14@gmail.com>
2025-07-31 10:27:56 -0600

    Reduccion de espaciado entre secciones

--- src/layout/Section.jsx
+++ src/layout/Section.jsx
@@ -1,6 +1,6 @@
 export function Section({ id, children, className = "" }) {
   const baseClasses =
-    "w-full min-h-screen p-4 md:p-16 lg:p-24 py-16 md:py-26 lg:py-34";
+    "w-full min-h-screen p-4 md:p-16 lg:p-24 py-16 md:py-22 lg:py-30";
 
   const combinedClassName = `${baseClasses} ${className}`;
 
