2086e385e28951e0ed2a63579e5760ab30042509
JoseLuis1411 <perezlarajose14@gmail.com>
2025-07-31 16:15:28 -0600

    Correccion de un titulo que se salta niveles

--- src/components/atomos/CuadriculaTecnologia.jsx
+++ src/components/atomos/CuadriculaTecnologia.jsx
@@ -4,7 +4,7 @@ import { normalizar } from "../../utilidades/normalizarIconos.js";
 export function CuadriculaTecnologia({ titulo, tecnologias }) {
   return (
     <div className="h-[300px] bg-background-primary-light dark:bg-background-primary-dark flex flex-col items-center p-4">
-      <h4 className="text-lg font-semibold">{titulo}</h4>
+      <h2 className="text-lg font-semibold">{titulo}</h2>
       <div className="flex w-full flex-wrap justify-center items-center gap-5 mt-7">
         {tecnologias.map((tecnologia, index) => {
           const tecnologiaNormalizada = normalizar(tecnologia);
