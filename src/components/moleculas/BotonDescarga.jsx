f7f1d75084e9b4dd7d27cbdbd14a7b391837c41c
JoseLuis1411 <perezlarajose14@gmail.com>
2025-08-29 16:35:07 -0600

    Se agrego mensaje de desacargar inciada al desacrgar el cv

--- src/components/moleculas/BotonDescarga.jsx
+++ src/components/moleculas/BotonDescarga.jsx
@@ -4,6 +4,7 @@ import {
 } from "../../constantes/styles.js";
 import clsx from "clsx";
 import { Icono } from "../atomos/Icono.jsx";
+import toast from "react-hot-toast"; // 👈 1. Importa la función toast
 
 export function BotonDescarga({
   href,
@@ -19,6 +20,17 @@ export function BotonDescarga({
     className
   );
 
+  const handleDownload = () => {
+    toast.success("Descarga iniciada.", {
+      duration: 4000,
+      style: {
+        background: "var(--color-accent-primary-light)",
+        color: "var(--color-text-button-light)",
+      },
+      icon: "📥",
+    });
+  };
+
   return (
     <a
       href={href}
@@ -26,6 +38,7 @@ export function BotonDescarga({
       target="_blank"
       rel="noopener noreferrer"
       {...rest}
+      onClick={handleDownload}
     >
       {iconName && <Icono nombre={iconName} tamaño={24} color="currentColor" />}
       {children}
